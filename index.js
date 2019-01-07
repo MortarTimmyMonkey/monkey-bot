192d25fb0b4d17fb779178f5fd88ce3527d19184
MortarTimmyMonkey <46425741+MortarTimmyMonkey@users.noreply.github.com>
2019-01-07 11:53:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const Commando = 
+require('discord.js-commando')
+const Discord = require('discord.js')
+const bot = new Commando.Client({
+    owner: '531199319387275295'
+});
+
+bot.registry.registerGroup('fun', 'Fun');
+bot.registry.registerGroup('misc', 'Misc');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + 
+'/commands');
+
+bot.on('message',function(message){
+  if(message.content == "Monkeys Lol")
+  {
+      message.channel.send("MONKEYS!")
+  }
+});
+
+bot.on('ready',function(){
+      console.log("Hello world!")
+})
+
+bot.on('error', console.error);
+ 
+
+bot.login('NTMxMzQzMDA1NzUyMjI5ODg4.DxPXBg.Zp840GB8bbPWzEpjTyOpWT0UTP0')
